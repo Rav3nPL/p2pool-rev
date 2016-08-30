5f27e136eb45fe6901c7fd02c463b7e1af1b3b3e
RavenPL <rav3n.pl@gmail.com>
2016-08-30 23:27:17 +0200

    add testnet

---
+++ p2pool/bitcoin/networks/revcoin_testnet.py
@@ -0,0 +1,28 @@
+import os
+import platform
+
+from twisted.internet import defer
+
+from .. import data, helper
+from p2pool.util import pack
+
+
+P2P_PREFIX = '81abaa68'.decode('hex')
+P2P_PORT = 17571
+ADDRESS_VERSION = 111
+RPC_PORT = 17572
+RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            (yield helper.check_genesis_block(bitcoind, '00000000053db42cc4eb4f24cf78f26ac099557065043ba9651680c32eb349be')) and
+            (yield bitcoind.rpc_getinfo())['testnet']
+        ))
+SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
+POW_FUNC = data.hash256
+BLOCK_PERIOD = 600 # s
+SYMBOL = 'RVCt'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'revcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/revcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.revcoin'), 'revcoin.conf')
+BLOCK_EXPLORER_URL_PREFIX = ''
+ADDRESS_EXPLORER_URL_PREFIX = ''
+TX_EXPLORER_URL_PREFIX = ''
+SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
+DUMB_SCRYPT_DIFF = 1
+DUST_THRESHOLD = 0.001e8
---
+++ p2pool/networks/revcoin_testnet.py
@@ -0,0 +1,27 @@
+from p2pool.bitcoin import networks
+
+# CHAIN_LENGTH = number of shares back client keeps
+# REAL_CHAIN_LENGTH = maximum number of shares back client uses to compute payout
+# REAL_CHAIN_LENGTH must always be <= CHAIN_LENGTH
+# REAL_CHAIN_LENGTH must be changed in sync with all other clients
+# changes can be done by changing one, then the other
+
+PARENT = networks.nets['revcoin_testnet']
+SHARE_PERIOD = 10 # seconds
+CHAIN_LENGTH = 60*60//10 # shares
+REAL_CHAIN_LENGTH = 60*60//10 # shares
+TARGET_LOOKBEHIND = 5 # shares
+SPREAD = 3 # blocks
+IDENTIFIER = '1122334455667788'.decode('hex')
+PREFIX = 'aabbccddeeff0011'.decode('hex')
+P2P_PORT = 18571
+MIN_TARGET = 1
+MAX_TARGET = 2**256//2**32 - 1
+PERSIST = False
+WORKER_PORT = 18572
+BOOTSTRAP_ADDRS = ''.split(' ')
+ANNOUNCE_CHANNEL = ''
+VERSION_CHECK = lambda v: None if 70000 <= v else 'Bitcoin version too old. Upgrade to 0.11.2 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
+VERSION_WARNING = lambda v: None
+#SOFTFORKS_REQUIRED = set(['bip65', 'csv'])
+#MINIMUM_PROTOCOL_VERSION = 1600
